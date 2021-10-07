7a12505369209dee8737a33daccecad189f6010b
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-07 23:30:00 +0530

    ADDED: regex to validate TLD optional field of email

--- email.js
+++ email.js
@@ -1,5 +1,5 @@
 function emailValidator(email){
-    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(([+-_.][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]*)[.]([a-z]{2,})");
+    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(([+-_.][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]*)[.]([a-z]{2,})(([.][a-zA-z]{2})?)");
     if(emailRegex.test(email))
         console.log("Valid");
     else throw 'email is not valid';
@@ -10,6 +10,7 @@ emailArr.push("abc_");
 emailArr.push("arp@gmail")
 emailArr.push("arp@gmail.com")
 emailArr.push("arp.xyz@gmail.com")
+emailArr.push("arp.xa_@gmail.com.in")
 emailArr.forEach(element => {
     try{
         emailValidator(element);
