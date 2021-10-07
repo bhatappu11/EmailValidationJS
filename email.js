199a7ff504a5bfa927cf97b69b4d737604c13797
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-07 23:24:34 +0530

    ADDED: regex to validate '.' and third mandatory part of email

--- email.js
+++ email.js
@@ -1,5 +1,5 @@
 function emailValidator(email){
-    let emailRegex = RegExp("[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]@[a-zA-Z0-9]*");
+    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(@[a-zA-Z0-9]*)[.]([a-z]{2,})");
     if(emailRegex.test(email))
         console.log("Valid");
     else throw 'email is not valid';
@@ -8,6 +8,7 @@ function emailValidator(email){
 let emailArr = new Array();
 emailArr.push("abc_");
 emailArr.push("arp@gmail")
+emailArr.push("arp@gmail.com")
 emailArr.forEach(element => {
     try{
         emailValidator(element);
