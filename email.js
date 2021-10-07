3c43d2e26679560ed2e76492f64ca47042085cc3
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-07 23:27:32 +0530

    ADDED: regex to validate optional part of the email

--- email.js
+++ email.js
@@ -1,5 +1,5 @@
 function emailValidator(email){
-    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(@[a-zA-Z0-9]*)[.]([a-z]{2,})");
+    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(([+-_.][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]*)[.]([a-z]{2,})");
     if(emailRegex.test(email))
         console.log("Valid");
     else throw 'email is not valid';
@@ -9,6 +9,7 @@ let emailArr = new Array();
 emailArr.push("abc_");
 emailArr.push("arp@gmail")
 emailArr.push("arp@gmail.com")
+emailArr.push("arp.xyz@gmail.com")
 emailArr.forEach(element => {
     try{
         emailValidator(element);
